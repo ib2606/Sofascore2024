9e0b1090f0b36d1fd5cce68575ad6980251d5b44
IvanBrkan <ivan.brkan@fer.hr>
2024-03-05 22:44:05 +0100

    inital Xcode project

---
+++ Sofascore2024/Sofascore2024/Sofascore2024App.swift
@@ -0,0 +1,17 @@
+//
+//  Sofascore2024App.swift
+//  Sofascore2024
+//
+//  Created by Ivan Brkan on 05.03.2024..
+//
+
+import SwiftUI
+
+@main
+struct Sofascore2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
